da0d3bf4a5b4a2aa9f2eef025142743fdb18e8cf
chidionyema <chidio@risual.com>
2025-02-03 23:41:06 +0000

    latest

--- src/theme/mixins.ts
+++ src/theme/mixins.ts
@@ -1,7 +1,9 @@
+import { alpha } from "@mui/material";
+import { PALETTE } from "./palette";
 
 // theme/mixins.ts
 export const cyberGlass = (intensity: number) => ({
-    backgroundColor: alpha(PALETTE.deepBlue, intensity),
-    backdropFilter: THEME_VARS.blur.backdrop,
-    border: `1px solid ${alpha(PALETTE.techBlue, 0.1)}`,
+    backgroundColor: alpha(PALETTE.dark.primary, intensity),
+    backdropFilter: 'blur(8px)',
+    border: `1px solid ${alpha(PALETTE.dark.secondary, 0.1)}`,
   });
